ffe20a199c4c73732506d542c2d6fa59f58112f7
zu21un <jwjoo03@gmail.com>
2025-07-24 10:30:59 +0900

    [leetcode] 274 fixed my solution

--- leetcode/Top_Interview_150/Medium_274_H-Index/my_solution.py
+++ leetcode/Top_Interview_150/Medium_274_H-Index/my_solution.py
@@ -2,12 +2,12 @@ class Solution:
     def hIndex(self, citations: List[int]) -> int:
         citation_cnt = len(citations)
         sorted_citations = sorted(citations)
-        max_value = 0
 
         for i in range(citation_cnt):
             paper = citation_cnt - i
             citation = sorted_citations[i]
             
-            max_value = max(max_value, min(paper, citation))
+            if paper <= citation:
+                return paper
 
-        return max_value
+        return 0
